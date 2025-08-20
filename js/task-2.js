382bc955db03f5a1c52f85d6e3b33c0afee6e19a
Anora888 <anoramaomao@gmail.com>
2025-08-20 10:17:48 +0300

    add files message

--- js/task-2.js
+++ js/task-2.js
@@ -1,5 +1,5 @@
 function formatMessage(message, maxLength) {
-    if (messege <= maxLength){
+    if (message <= maxLength){
         return message;
     } else {
         return message.slice(0, maxLength) + "...";
